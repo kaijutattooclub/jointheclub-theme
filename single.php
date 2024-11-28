ea43486c1c11ab461021263fa1a28ed8bf096f4a
Mikkel Rask <35899030+mikkelrask@users.noreply.github.com>
2024-11-28 18:06:47 +0100

    Guest artist styling (#24)
    * Fix: "Current and upcoming guests" headline shown
    when no current or upcoming guests are announced
    
    * UI: Add badge to guest artist entry
    
    * UI: Add artist style to guest artists entry
    
    * Redirect if user lands on artist page, and artist is a guest
    
    * UI: 404 page styling - added lottieanimation of a 404-dino
    
    * UI: Improve guest artist display logic and styling
    
    * UI: Adjust styling for images and captions, refine padding and alignment
    
    * UI: Update .gitignore to exclude additional file types and modify package.json bundle command; enhance guest artist page layout and styling
    
    ---------
    
    Co-authored-by: Mikkel Rask <mikkelrask@pm.me>

--- single.php
+++ single.php
@@ -16,7 +16,7 @@ if (is_page('gaestetattovoerer-i-kaiju-tattoo-club')) {
 ?>
 <style>p{margin-bottom: 1.2rem;} main ul{margin-bottom:1.1rem;} main ul li{padding-left:1.2rem; list-style: inside; } h2{margin-bottom:1.2rem;} </style>
 	<main id="primary" class="site-main">
-	<div id="post-<?php the_ID();?>" class="container my-4 rounded overflow-hidden flex flex-col mx-auto p-4">
+	<div id="post-<?php the_ID();?>" class="container my-8 rounded overflow-hidden flex flex-col mx-auto p-4">
 
         <div class="relative">
             <a href="<?php the_permalink();?>">
@@ -26,12 +26,20 @@ if (is_page('gaestetattovoerer-i-kaiju-tattoo-club')) {
             </a>
 
         </div>
-        <h1 class="text-4xl py-4"><?php the_title();?></h1>
-        <div class="pb-5 text-sm font-regular text-gray-900 flex">
-            <span class="mr-3 flex flex-row items-center text-white">
-                Kategori:<span class="ml-1 text-primary"><?php echo "#", strip_tags(get_the_category_list(", #"));?></span></span>
+        <h1 class="text-2xl py-4"><?php the_title();?></h1>
+        <div class="text-sm font-regular text-gray-900 flex-col">
+            <p class="mr-3 flex flex-row items-center text-white">
+                Kategori: <span class=" text-primary font-bold"><?php echo "#", strip_tags(get_the_category_list(", #"));?></span>
+            </p>
+            <p class="mr-3 flex flex-row items-center text-white">
+                Skrevet af: <?php 
+                    global $post;
+                    $author_id = $post->post_author;
+                    echo esc_html(get_the_author_meta('display_name', $author_id));
+                ?>
+            </p>
         </div>
-        <hr class="my-2">
+        <hr class="mb-4">
         <?php the_content(); ?>
 
 	</main><!-- #main -->
